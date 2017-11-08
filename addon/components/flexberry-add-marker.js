8fc9e68c00651ff3091fd3c303caa7f7e10a0e7f
Semyon Volkov <Volkovsema@gmail.com>
2017-11-08 19:27:26 +0500

    Some design changes

--- addon/components/flexberry-add-marker.js
+++ addon/components/flexberry-add-marker.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 import layout from '../templates/components/flexberry-add-marker';
 
 export default Ember.Component.extend({
-  layout
+  layout,
+
+  dropDownClass: 'fluid',
+
+  signTypes: ['адин', 'два', 'пять'],
+
+  signViews: ['два', 'три']
 });
