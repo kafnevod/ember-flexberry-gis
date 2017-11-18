57f66a14622a62e4f08b4170e82fc8c53fb9cdf9
Alexandr Vinnik <avinnik@ics.perm.ru>
2017-11-18 23:07:11 +0500

    Add drop-down component

---
+++ addon/components/gradients/dropdown-gradient.js
@@ -0,0 +1,25 @@
+/**
+  @module ember-flexberry-gis
+*/
+
+import Ember from 'ember';
+import layout from '../../templates/components/gradients/dropdown-gradient';
+
+/**
+  Component for gradient editing.
+
+  @class GradientEditComponent
+  @extends <a href="http://emberjs.com/api/classes/Ember.Component.html">Ember.Component</a>
+*/
+
+export default Ember.Component.extend({
+  /**
+      Reference to component's template.
+  */
+  layout,
+
+  didInsertElement() {
+    this.$('.ui.dropdown')
+          .dropdown();
+  }
+});
---
+++ addon/components/gradients/main-gradient.js
@@ -0,0 +1,20 @@
+/**
+  @module ember-flexberry-gis
+*/
+
+import Ember from 'ember';
+import layout from '../../templates/components/gradients/main-gradient';
+
+/**
+  Component for gradient editing.
+
+  @class GradientEditComponent
+  @extends <a href="http://emberjs.com/api/classes/Ember.Component.html">Ember.Component</a>
+*/
+
+export default Ember.Component.extend({
+  /**
+      Reference to component's template.
+  */
+  layout
+});
