3c886a16c03b99c35b1bab0d8efccfd55a7819e3
Serega Matin <seregamatin@gmail.com>
2019-08-06 13:35:10 +0500

    Fix 'flexberry-map' unit tests

--- tests/unit/components/flexberry-map-test.js
+++ tests/unit/components/flexberry-map-test.js
@@ -4,7 +4,10 @@ import { moduleForComponent, test } from 'ember-qunit';
 moduleForComponent('flexberry-map', 'Unit | Component | flexberry map', {
   unit: true,
   needs: [
-    'component:flexberry-layers'
+    'component:flexberry-layers',
+    'map-tool:drag',
+    'service:i18n',
+    'config:environment'
   ]
 });
 
@@ -48,9 +51,11 @@ test('should pass center/zoom from properties to leaflet map', function (assert)
   assert.equal(leafletMap.getZoom(), 0);
 
   // after update to leaflet-1.0.0 panTo not directly change center,
-  // it will changed after animation will trigger moveend
+  // it will changed after animation will trigger 'moveend' event.
   let promise = new Ember.Test.promise((resolve) => {
-    leafletMap.on('moveend', resolve);
+    leafletMap.on('moveend', () => {
+      setTimeout(resolve, 500);
+    });
   });
 
   Ember.run(() => {
